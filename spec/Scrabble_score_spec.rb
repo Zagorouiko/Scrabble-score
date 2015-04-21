256c0824b55ba86a1a526a8fd316cc74f9b4a75f
Leon <student@epicodus.com>
2015-04-21 14:48:02 -0700

    Returns a scrabble score for a single word, test pass.

--- spec/Scrabble_score_spec.rb
+++ spec/Scrabble_score_spec.rb
@@ -4,7 +4,7 @@
  require("Scrabble_score")
 
  describe('Fixnum#Scrabble_score') do
-   it('Returns a scrabble score for a single letter') do
+   it('Returns a scrabble score for a single word') do
      expect("cat".Scrabble_score()).to(eq(5))
    end
  end
